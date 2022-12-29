3898466335293b60fb98f3b47be9d1f0ffe96ee2
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2022-12-29 12:17:30 +0100

    Ajout de la machine à états avec registre a décalage

--- CHAINE_DE_TRANSMISSION_SERIE/TX.cpp
+++ CHAINE_DE_TRANSMISSION_SERIE/TX.cpp
@@ -1,1 +1,40 @@
 #include "TX.h"
+
+void Tx::transmettre(){
+    
+
+    switch(etat){
+        case E_wait:
+            // On attend la réception d'un octet
+            tmp = input.read();
+            if(tmp == NULL){
+                TxD.write(SC_LOGIC_1);
+            }
+            else{
+                etat = E_send;
+                TxD.write(SC_LOGIC_0);
+                //XOR sur tout les bits pour détéminer la parité 
+                parity = tmp.xor_reduce();
+                i = 0;
+            }
+            break;
+        case E_send:
+            if(i < 8){
+                //registre a décalge pour envoyer les bits un par un 
+                TxD.write(tmp[7]);
+                tmp.range(7,1) = tmp.range(6,0);
+                tmp[0] = 0;
+            }
+            else{
+                etat = E_parite;
+                //Envoi du bit de parité a la fin
+                TxD.write(parity);
+            }
+            break;
+        case E_parite:
+            // Retour a l'état attente 
+            etat = E_wait;
+            TxD.write(SC_LOGIC_1);
+            break;
+    }
+}
--- CHAINE_DE_TRANSMISSION_SERIE/TX.h
+++ CHAINE_DE_TRANSMISSION_SERIE/TX.h
@@ -3,9 +3,30 @@
 
 #include "systemc.h"
 
+typedef
+  enum {E_wait,E_send,E_parite} t_etat;
+
 SC_MODULE(Tx){
 
+    //Declaration de I/O
+    sc_fifo_in<sc_lv<8>> input;
+    sc_fifo_out<sc_logic> TxD;
+    sc_in<bool> clock;
 
+    //Declaration des etats
+    t_etat etat;
 
+    sc_lv<8> tmp;
+    sc_logic parity;
+    sc_int<8> i;
+
+    void transmettre();
+
+    SC_CTOR(Tx){
+
+        etat = E_wait;
+        SC_THREAD(transmettre);
+        sensitive << clock.pos();
+    }
 };
 #endif
